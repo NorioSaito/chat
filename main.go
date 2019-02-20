7ccf9e9576dd7594e9c34ce8fd732d86219ece84
Norio Saito <s.norio.rockin@gmail.com>
2019-02-20 22:39:23 +0900

    ログイン処理実装中

--- main.go
+++ main.go
@@ -24,11 +24,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var addr = flag.String("addr", "8080 ", "アプリケーションのアドレス")
+	var addr = flag.String("addr", ":8080 ", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します。
 	r := newRoom()
 	// r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// チャットルームを開始します。
 	go r.run()
