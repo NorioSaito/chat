c01c6e9798ee97db3915ea610ca3afc7d9c22545
Norio Saito <s.norio.rockin@gmail.com>
2019-02-21 22:17:55 +0900

    OAuth2実装中。とりあえずGoogleのみ

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -26,6 +29,9 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080 ", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します。
+	// Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("@Force2868")
+	gomniauth.WithProviders(google.New("1063896493054-duevn2cv6p7o066ltrv2ngo81ima742i.apps.googleusercontent.com", "FAC859Qe943W7jJjA8piIGNE", "http://localhost:8080/auth/callback/google"))
 	r := newRoom()
 	// r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
